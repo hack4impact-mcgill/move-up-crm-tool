652e9c4f2fd5d1c0bd73b369261ec32ba2ad7921
celinehuang <celine756@hotmail.com>
2019-10-30 17:45:50 -0400

    add mentor domain model class #18

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,3 +3,15 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # create model classes here
+
+
+class Mentor:
+
+    # contructor
+    def __init__(self, name, email, listOfClients):
+        self.name = name
+        self.email = email
+        self.listOfClients = []                 # creates a new empty list for each mentor
+
+    def add_client(self, client):
+        self.listOfClients.append(client)
