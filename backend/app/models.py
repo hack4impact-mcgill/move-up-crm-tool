1efe828cf09e9b4949b4306748b25003baa52ff7
MichelMajdalani <michel.majdalani@mail.mcgill.ca>
2019-11-07 13:16:31 -0500

    Added Donor domain model

--- backend/app/models.py
+++ backend/app/models.py
@@ -30,3 +30,16 @@ class Client:
             "notes": self.notes,
             "attachments": self.attachments
         }
+
+class Donor:
+
+    # constructor
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "email": self.email
+            }
