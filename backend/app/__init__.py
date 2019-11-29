ecd2acb6984c3e8f78362561e7552813ba656ef5
celinehuang <celine756@hotmail.com>
2019-11-28 23:38:02 -0500

    set up email templates and flask mail api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_mail import Mail
 
 from config import config
+
+mail = Mail()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    # set up Flask Mail extension
+    mail.init_app(app)
 
     from .main import main as main_blueprint
 
