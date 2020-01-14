43920c58de1a59107fbe606d1fe77c78c58a2425
Celine Huang <33935299+celinehuang@users.noreply.github.com>
2020-01-14 15:56:54 -0500

    Add route for sending emails (#34)
    * set up email templates and flask mail api
    
    * Add route and method for sending emails
    
    * Code review changes

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,16 @@ class Config:
     APP_NAME = "Move Up CRM Tool"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
 
+    # Email
+    EMAIL_SENDER = "hack4impactmcgilltest@gmail.com"
+    EMAIL_SUBJECT_PREFIX = "[{}]".format(APP_NAME)
+    MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_PORT = os.environ.get("MAIL_PORT", 587)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", True)
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", False)
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     @staticmethod
     def init_app(app):
         pass
@@ -22,6 +32,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     pass
+
 
 config = {
     "development": DevelopmentConfig,
