07b0572ba7440f77aa7958b4056952c43f064004
celinehuang <celine756@hotmail.com>
2020-02-13 18:53:33 -0500

    add the dashboard page and empty pages for routing

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -3,6 +3,30 @@ const routes = [
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/Index.vue") }]
+  },
+  {
+    path: "/home",
+    component: () => import("layouts/MainLayout.vue"),
+    // meta: { requiresAuth: true },
+    children: [{ path: "", component: () => import("pages/Dashboard.vue") }]
+  },
+  {
+    path: "/clients",
+    component: () => import("layouts/MainLayout.vue"),
+    // meta: { requiresAuth: true },
+    children: [{ path: "", component: () => import("pages/Clients.vue") }]
+  },
+  {
+    path: "/donors",
+    component: () => import("layouts/MainLayout.vue"),
+    // meta: { requiresAuth: true },
+    children: [{ path: "", component: () => import("pages/Donors.vue") }]
+  },
+  {
+    path: "/volunteers",
+    component: () => import("layouts/MainLayout.vue"),
+    // meta: { requiresAuth: true },
+    children: [{ path: "", component: () => import("pages/Volunteers.vue") }]
   }
 ];
 
