9b8bc50ce2f69d04fbdc3210ad0159304df7b368
MichelMajdalani <michel.majdalani@mail.mcgill.ca>
2020-03-24 11:53:40 -0400

    Updated models to include donor and improved reusability

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,15 +1,15 @@
 import json
-from werkzeug.security import generate_password_hash, check_password_hash
 
 # create model classes here
 class User:
     # constructor
-    def __init__(self, name, email):
+    def __init__(self, name, email, id_number):
         self.name = name
         self.email = email
+        self.id_number = id_number
 
     def serialize(self):
-        return {"name": self.name, "email": self.email}
+        return {"name": self.name, "email": self.email, "id": self.id_number}
 
 
 class Mentor(User):
@@ -17,38 +17,38 @@ class Mentor(User):
 
 
 class Client(User):
-
     # constructor
-    def __init__(self, name, email, notes, attachments):
-        super().__init__(name, email)
+    def __init__(self, name, email, id_number, notes, attachments):
+        super().__init__(name, email, id_number)
         self.notes = notes
         self.attachments = attachments
-
+    
     def serialize(self):
-        return {
-            "name": self.name,
-            "email": self.email,
-            "notes": self.notes,
-            "attachments": self.attachments,
-        }
+        new_dict = super().serialize()
+        new_dict.update({"notes": self.notes, "attachments": self.attachments})
+        return new_dict
 
 
 class Volunteer(User):
-    
     #constructor
-    def __init__(self, name, email, notes, attachments):
-        super().__init__(name, email)
+    def __init__(self, name, email, id_number, notes, attachments):
+        super().__init__(name, email, id_number)
         self.notes = notes
         self.attachments = attachments
 
     def serialize(self):
-        return {
-            "name": self.name,
-            "email": self.email,
-            "notes": self.notes,
-            "attachments": self.attachments,
-        }
+        new_dict = super().serialize()
+        new_dict.update({"notes": self.notes, "attachments": self.attachments})
+        return new_dict
 
 
 class Donor(User):
-    pass
+    def __init__(self, name, email, id_number, notes, total_donated):
+        super().__init__(name, email, id_number)
+        self.notes = notes
+        self.total_donated = total_donated
+
+    def serialize(self):
+        new_dict = super().serialize()
+        new_dict.update({"notes": self.notes, "total_donated": self.total_donated})
+        return new_dict
