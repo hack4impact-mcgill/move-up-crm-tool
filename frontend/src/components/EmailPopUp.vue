28af3f70bf6f578ef8e961a4d3b7618fc336446a
celinehuang <celine756@hotmail.com>
2020-05-24 17:13:09 -0400

    Add filtering for selecting email from client email list

--- frontend/src/components/EmailPopUp.vue
+++ frontend/src/components/EmailPopUp.vue
@@ -10,10 +10,12 @@
         <q-select
           v-model="selectedEmails"
           multiple
-          :options="allClientEmails"
+          :options="filterOptions"
           use-chips
           use-input
           input-debounce="0"
+          @new-value="createValue"
+          @filter="filterFn"
           stack-label
           label="To"
         />
@@ -49,28 +51,43 @@ export default {
     this.selected.forEach(element => {
       this.selectedEmails.push(element.email);
     });
-    // this.allEmails.forEach(e => {
-    //   this.allClientEmails.push(e);
-    // });
     this.allClientEmails = this.allEmails;
-    console.log("hello " + this.allClientEmails);
   },
+
   methods: {
-    log(email) {
-      console.log(`${email} has been removed`);
+    createValue(val, done) {
+      // Calling done(var) when new-value-mode is not set or is "add", or done(var, "add") adds "var" content to the model
+      // and it resets the input textbox to empty string
+      // ----
+      // Calling done(var) when new-value-mode is "add-unique", or done(var, "add-unique") adds "var" content to the model
+      // only if is not already set and it resets the input textbox to empty string
+      // ----
+      // Calling done(var) when new-value-mode is "toggle", or done(var, "toggle") toggles the model with "var" content
+      // (adds to model if not already in the model, removes from model if already has it)
+      // and it resets the input textbox to empty string
+      // ----
+      // If "var" content is undefined/null, then it doesn't tampers with the model
+      // and only resets the input textbox to empty string
+
+      if (val.length > 0) {
+        if (!this.allClientEmails.includes(val)) {
+          this.allClientEmails.push(val);
+        }
+        done(val, "toggle");
+      }
+    },
+    filterFn(val, update) {
+      update(() => {
+        if (val === "") {
+          this.filterOptions = this.allClientEmails;
+        } else {
+          const needle = val.toLowerCase();
+          this.filterOptions = this.allClientEmails.filter(
+            v => v.toLowerCase().indexOf(needle) > -1
+          );
+        }
+      });
     }
-    // filterFn(val, update) {
-    //   update(() => {
-    //     if (val === "") {
-    //       this.filterOptions = this.allClientEmails;
-    //     } else {
-    //       const needle = val.toLowerCase();
-    //       this.filterOptions = this.allClientEmails.filter(
-    //         v => v.toLowerCase().indexOf(needle) > -1
-    //       );
-    //     }
-    //   });
-    // }
   }
 };
 </script>
