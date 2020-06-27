3efe90f04baf9246dcbfa62035035b3c566b5d2f
Celine Huang <33935299+celinehuang@users.noreply.github.com>
2020-06-27 11:29:46 -0400

    Update backend manager to specify which config to run (#74)
    * Update backend manager to specify which config to run
    
    * Import OS library
    
    * Add comments and missing flask migration manager
    
    * Fix conditional bug causing test to fail
    
    * Remove unnecessary db migration libraries and code

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_mail import Mail
+import os
 from flask_cors import CORS
 from config import config
 from flask_cors import CORS
@@ -10,12 +11,17 @@ mail = Mail()
 def create_app(config_name):
     app = Flask(__name__)
     CORS(app)
+
+    if config_name is None:
+        config_name = os.getenv("FLASK_CONFIG") or "default"
+
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     # set up Flask Mail extension
     mail.init_app(app)
 
+    # create app blueprint
     from .main import main as main_blueprint
 
     app.register_blueprint(main_blueprint)
